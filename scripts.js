d8b262e20d178b46d5d099a59e6d498c16242178
sierpien <stepnikagata@gmail.com>
2020-06-08 12:31:26 +0200

    first excercise

---
+++ scripts.js
@@ -0,0 +1,31 @@
+const products = [
+    {name:'Iphone',price:200},
+    {name:'Motorola',price:70},
+    {name:'Samsung',price:150},
+    {name:'Sony',price:98},
+    {name:'Windows pone',price:10}
+ ];
+
+ let template = '';
+
+ 
+ products.forEach(function(product){
+
+    function discount(){
+        if(product.price < 100){
+            return `<span>On sale !!</span>`;
+        }
+        return ``;
+    }
+
+    template += `
+    <div class="product">
+        <h1>${product.name}</h1>
+        <strong>Price: $ ${product.price} </strong>
+        ${discount()}
+    </div>`
+   
+ });
+
+ document.body.insertAdjacentHTML('afterbegin', template);
+
